6e4acaaed9c5292f9bdb8251ae6603eedc0d6030
Sumanth Hegde <74821622+sumanthhegde55@users.noreply.github.com>
2022-02-21 14:02:43 +0530

    add script for secret creation

---
+++ script.sh
@@ -0,0 +1,9 @@
+ACCOUNT=$(aws sts get-caller-identity --query 'Account' --output text) #aws account number
+REGION=ap-south-1                                     #aws ECR region
+SECRET_NAME=${REGION}-ecr-registry                    #secret_name
+EMAIL=sumanthhegde55@gmail.com                                     #can be anything
+
+TOKEN=`aws ecr --region=$REGION get-authorization-token --output text --query authorizationData[].authorizationToken | base64 -d | cut -d: -f2`
+
+kubectl delete secret --ignore-not-found $SECRET_NAME
+kubectl create secret docker-registry $SECRET_NAME --docker-server=https://$ACCOUNT.dkr.ecr.${REGION}.amazonaws.com --docker-username=AWS --docker-password="${TOKEN}" --docker-email="${EMAIL}" --dry-run=client --output=yaml > secret3.yaml
