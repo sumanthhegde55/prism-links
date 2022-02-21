e202cc08fe8b9afc497b0c89dba23cd7b424a9ac
Sumanth Hegde <74821622+sumanthhegde55@users.noreply.github.com>
2022-02-21 14:03:56 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 ACCOUNT=$(aws sts get-caller-identity --query 'Account' --output text) #aws account number
 REGION=ap-south-1                                     #aws ECR region
 SECRET_NAME=${REGION}-ecr-registry                    #secret_name
-EMAIL=sumanthhegde55@gmail.com                                     #can be anything
+EMAIL=xyz@gmail.com                                     #can be anything
 
 TOKEN=`aws ecr --region=$REGION get-authorization-token --output text --query authorizationData[].authorizationToken | base64 -d | cut -d: -f2`
 
